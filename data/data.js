c2f2a4c6c70007b7bc6b7fde54662e2796e4ed04
Sebastian De Ro <sebastian.dero@yahoo.de>
2018-05-19 14:33:13 +0200

    cleanup and load from file

---
+++ data/data.js
@@ -0,0 +1,34 @@
+var data = {
+    "chart": {
+        "container": "#OrganiseChart-simple"
+    },
+    "nodeStructure": {
+        "text": {
+            "name": "Parent node"
+        },
+        "children": [
+            {
+                "text": {
+                    "name": "First child"
+                },
+                "children": [
+                    {
+                        "text": {
+                            "name": "First child"
+                        }
+                    },
+                    {
+                        "text": {
+                            "name": "Second child"
+                        }
+                    }
+                ]
+            },
+            {
+                "text": {
+                    "name": "Second child"
+                }
+            }
+        ]
+    }
+}
