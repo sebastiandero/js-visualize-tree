4bec8031af9a1463225d0684545648afaf62bf1b
Sebastian De Ro <sebastian.dero@yahoo.de>
2018-05-19 16:20:31 +0200

    custom styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+const tree_chart_conf = {
+    chart: {
+        container: "#tai-map-tree",
+        node: {
+            collapsable: true
+        },
+        connectors: {
+            type: 'step'
+        },
+        animation: {
+            nodeAnimation: "easeOutCubic",
+            nodeSpeed: 300,
+            connectorsAnimation: "cubic",
+            connectorsSpeed: 300
+        }
+    },
+    nodeStructure: data
+}
 
+let tree_chart = new Treant(tree_chart_conf);
